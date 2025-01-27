9cddbc42944cd832eba0e6799abc9cd406b2f9da
magicdj12 <moha.fake@gmail.com>
2025-01-27 03:36:36 +0330

    Update __init__.py

--- RAUSHAN/__init__.py
+++ RAUSHAN/__init__.py
@@ -11,26 +11,41 @@ from pyrogram import Client
 import config
 from RAUSHAN.modules import all_modules
 
+# تنظیمات لاگینگ
 logging.basicConfig(
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
     datefmt="%d-%b-%y %H:%M:%S",
-    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    handlers=[
+        logging.FileHandler("log.txt"),
+        logging.StreamHandler()
+    ],
     level=logging.INFO,
 )
+
+# کاهش سطح لاگ‌های pyrogram
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 
-boot = time.time()
+# متغیرهای اصلی
+boot = time.time()  # زمان شروع ربات
+
+# اتصال به دیتابیس
 mongo = MongoCli(config.MONGO_URL)
 db = mongo.Anonymous
 
-
+# تنظیمات دسترسی
 OWNER = config.OWNER_ID
 # DEVS = config.SUDO_USERS | config.OWNER_ID
 
 
 class AMBOT(Client):
+    """
+    کلاس اصلی ربات که از Client پایروگرام ارث‌بری می‌کند
+    """
     def __init__(self):
+        """
+        مقداردهی اولیه کلاس ربات
+        """
         super().__init__(
             name="AMBOT",
             api_id=config.API_ID,
@@ -40,16 +55,28 @@ class AMBOT(Client):
         )
 
     async def start(self):
+        """
+        شروع ربات و دریافت اطلاعات آن
+        """
         await super().start()
         get_me = await self.get_me()
+        
+        # ذخیره اطلاعات ربات
         self.id = get_me.id
         self.name = get_me.mention
         self.username = get_me.username
+        
+        LOGGER.info(f"ربات {self.name} شروع به کار کرد")
 
     async def stop(self):
+        """
+        توقف ربات
+        """
         await super().stop()
+        LOGGER.info("ربات متوقف شد")
 
 
+# ایجاد نمونه برای توسعه
 dev = Client(
     "Dev",
     bot_token=config.BOT_TOKEN,
@@ -58,11 +85,20 @@ dev = Client(
     # plugins=dict(root="RAUSHAN.modules"),
 )
 
+# شروع کلاینت توسعه
 dev.start()
 
+# دریافت اطلاعات ربات
 BOT_ID = config.BOT_TOKEN.split(":")[0]
-x = dev.get_me()
-BOT_NAME = x.first_name + (x.last_name or "")
-BOT_USERNAME = x.username
-BOT_MENTION = x.mention
-BOT_DC_ID = x.dc_id
+bot_info = dev.get_me()
+BOT_NAME = bot_info.first_name + (bot_info.last_name or "")
+BOT_USERNAME = bot_info.username
+BOT_MENTION = bot_info.mention
+BOT_DC_ID = bot_info.dc_id
+
+LOGGER.info(f"""اطلاعات ربات:
+نام: {BOT_NAME}
+نام کاربری: @{BOT_USERNAME}
+شناسه: {BOT_ID}
+دیتاسنتر: {BOT_DC_ID}
+""")
